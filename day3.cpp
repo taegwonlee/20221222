71b6342bf68ba1231824d9c94816a8870fcb513b
taegwonlee <vlldbllv@naver.com>
2021-12-22 14:59:47 +0900

    두번째

--- day3.cpp
+++ day3.cpp
@@ -4,20 +4,8 @@ using namespace std;
 
 int main()
 {
-
-	int Height = 0;						
-		int Base = 0;
-
-	float Area = 0.0f;						
-
-	cin >> Height;
-	cin >> Base;
-
-	Area = (static_cast<float>(Height) * static_cast<float>(Base)) / 2.0f;	
-	
-	cout << Area << endl;
-
-	cout << "git »ý¼º" << endl;
+	cout << "Hello World" << endl;
 
 	return 0;
+
 }
